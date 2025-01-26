57e4a7a8355ba90395654689b5cd446f02dd6bb2
Abena <priscillaabenaaboagye@gmail.com>
2025-01-26 17:43:45 +0000

    the main function

--- project.py
+++ project.py
@@ -15,4 +15,19 @@ def filter_expenses_by_category(expenses, category):
     return(filter(lambda expense: expense[category] == category, expenses))
 
 
-expenses = []
+
+def main():
+    expenses = []
+    while True:
+       print('\nExpense Tracker')
+       print('1. Add an expense')
+       print('2. List all expenses')
+       print('3. Show total expenses')
+       print('4. Filter expenses by category')
+       print('5. Exit')
+
+       choice = input("Enter your choice: ")
+       if choice == "1":
+        pass
+
+
