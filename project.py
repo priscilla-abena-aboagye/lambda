8f77f3111616d4859a5eb15813af5e3273d830f9
Abena <priscillaabenaaboagye@gmail.com>
2025-01-26 14:37:50 +0000

    the def function

---
+++ project.py
@@ -0,0 +1,10 @@
+def add_expense(expenses, amount, category):
+    expenses.append({"amount": amount, "category": category})
+
+
+def print_expenses(expenses):
+    for expense in expenses:
+        pass
+
+
+expenses = []
